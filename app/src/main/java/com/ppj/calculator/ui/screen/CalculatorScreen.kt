50c3ed654be9e7ce0ba025e0d23f5d4b5627f0c5
Moses <mosesramthlanmawia@gmail.com>
2026-05-02 16:46:48 +0530

    modified misc.xml gradele.kts and libs.version to match codes

--- app/src/main/java/com/ppj/calculator/ui/screen/CalculatorScreen.kt
+++ app/src/main/java/com/ppj/calculator/ui/screen/CalculatorScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ppj.calculator.viewmodel.CalculatorViewModel
 
 @Composable
